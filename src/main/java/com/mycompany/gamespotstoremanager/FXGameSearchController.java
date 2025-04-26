cbff85c8bad209027c874e75c52ae1d93d07baec
cliffy987 <ebwhitehead63@gmail.com>
2025-04-26 09:49:38 -0400

    Connected javafx elements to GameSearch controller

--- src/main/java/com/mycompany/gamespotstoremanager/FXGameSearchController.java
+++ src/main/java/com/mycompany/gamespotstoremanager/FXGameSearchController.java
@@ -1,14 +1,26 @@
 package com.mycompany.gamespotstoremanager;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
-public class FXGameSearchController {
+public class FXGameSearchController implements Initializable {
 
     @FXML private TextField nameField;
+    @FXML private TextField genreField;
+    @FXML private TextField publisherField;
+    @FXML private MenuButton ratingMenu;
+    @FXML private Text ratingText;
     
     private String nameEntered = "";
+    private String genreEntered = "";
+    private String publisherEntered = "";
     
     @FXML
     private void switchToMainMenu() throws IOException {
@@ -18,11 +30,29 @@ public class FXGameSearchController {
     @FXML
     private void nameFieldUpdated() {
         nameEntered = nameField.getText();
-        System.out.println(nameEntered);
+    }
+    
+    @FXML
+    private void genreFieldUpdated() {
+        genreEntered = genreField.getText();
+    }
+    
+    @FXML
+    private void publisherFieldUpdated() {
+        publisherEntered = publisherField.getText();
     }
     
     private void searchPressed() {
         //Just in case user didn't press "Enter"
         nameFieldUpdated();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        for (MenuItem menuItem : ratingMenu.getItems()) {
+            menuItem.setOnAction(e -> {
+                ratingText.setText(menuItem.getText());
+            });
+        }
     }
 }
